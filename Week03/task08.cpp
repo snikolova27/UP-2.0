846b98039e3a027c4b668a3feb771a797e6962a3
Sonya Nikolova <76953138+snikolova27@users.noreply.github.com>
2021-07-21 12:33:35 +0300

    Update task08.cpp

--- Week03/task08.cpp
+++ Week03/task08.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+const int MIN-DAY = 2;
+const int MAX-DAY = 22;
+const int DAYS-LEFT = 9;
 
 int main()
 {
     int n;
     std::cin >> n;
 
-    if (n > 2 && n < 22)
+    if (n > MIN-DAY && n <MAX-DAY)
     {
-        if (n <= 9)
+        if (n <= DAYS-LEFT)
         {
-            std::cout << "Students should return to school on " << 22 + n << " January 2006 \n";
+            std::cout << "Students should return to school on " << MAX-DAY + n << " January 2006 \n";
         }
         else
         {
-            std::cout << "Students should return to school on " << n - 9 << " February 2006 \n";
+            std::cout << "Students should return to school on " << n - DAYS-LEFT << " February 2006 \n";
         }
     }
     return 0;
