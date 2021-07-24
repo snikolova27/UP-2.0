9421671dcdc5785bfcfea51ff326eddbcb18dfc5
Sonya Nikolova <snikolova27@gmail.com>
2021-07-24 16:07:24 +0300

    add selection and insertion sort

--- Week07_Arrays/task06.cpp
+++ Week07_Arrays/task06.cpp
@@ -9,6 +9,22 @@ void swap(int &a, int &b)
     a = b;
     b = temp;
 }
+int minFrom(int *array, int size, int pos)
+{
+    int minEl = array[pos];
+    int minIdx = pos;
+
+    for (int i = pos + 1; i < size; i++)
+    {
+        if (array[i] < minEl)
+        {
+            minEl = array[i];
+            minIdx = i;
+        }
+    }
+
+    return minIdx;
+}
 
 int insertElements(int *array)
 {
@@ -40,13 +56,38 @@ void bubbleSort(int *array, int size)
 
     for (int i = 0; i < size - 1; i++)
     {
-        for(int j=0; j<size -1-i;j++)
+        for (int j = 0; j < size - 1 - i; j++)
         {
-            if(array[j] > array [j+1])
+            if (array[j] > array[j + 1])
             {
-                swap(array[j], array[j+1]);
+                swap(array[j], array[j + 1]);
             }
         }
+    }
+}
+
+void selectionSort(int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int pos = minFrom(array, size, i);
+        swap(array[i], array[pos]);
+    }
+}
+
+void insertionSort(int *array, int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int temp = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && temp <= array[j])
+        {
+            array[j + 1] = array[j];
+            j -= 1;
+        }
+        array[j + 1] = temp;
     }
 }
 int main()
@@ -54,8 +95,15 @@ int main()
 
     int numbers[MAX_ARRAY_SIZE] = {};
     int size = insertElements(numbers);
-    bubbleSort(numbers, size);
+    // bubbleSort(numbers, size);
+    // printArray(numbers, size);
+    // std::cout << std::endl;
+    // selectionSort(numbers, size);
+    // printArray(numbers, size);
+    std::cout << std::endl;
+    insertionSort(numbers, size);
     printArray(numbers, size);
+
 
     return 0;
 }
