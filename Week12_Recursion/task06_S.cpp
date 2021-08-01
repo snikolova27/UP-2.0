41589937cf693b6476c1ec86ee796428fafa405c
Sonya Nikolova <snikolova27@gmail.com>
2021-08-01 16:11:43 +0300

    add one more variant of isPrime function

--- Week12_Recursion/task06_S.cpp
+++ Week12_Recursion/task06_S.cpp
@@ -16,6 +16,23 @@ bool isPrimeHelper(int num, int i)
     return isPrimeHelper(num, i + 1);
 }
 
+bool isPrime(int n, int i = 2)
+{
+    if (n <= 2)
+    {
+        return (n == 2) ? true : false;
+    }
+    if (n % i == 0)
+    {
+        return false;
+    }
+    if (i * i > n)
+    {
+        return true;
+    }
+    return isPrime(n, i + 1);
+}
+
 bool isPrimeRec(int num)
 {
     return isPrimeHelper(num, 2);
